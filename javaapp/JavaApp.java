b4981717ad51ad99550eb4bd9171badab6e4ff67
ratheeshkv179 <ratheeshkv179@gmail.com>
2018-02-05 15:09:14 +0530

    simple java app

---
+++ javaapp/JavaApp.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapp;
+
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author ratkv
+ */
+public class JavaApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=0;i<10;i++){
+            System.out.println("Hello Ratheesh K V");
+        }
+        
+        
+    }
+    
+}
